8ac375011232135d75c834e59e5d1efa4a1873b8
rimrumrom <yr981030@naver.com>
2020-07-03 18:21:37 +0900

    2020-7-3 작업

--- public/aboutMe.php
+++ public/aboutMe.php
@@ -2,10 +2,23 @@
 include "../part/head_head.php";
 ?>
 <!--링크 거는 구간-->
+<link rel="stylesheet" href="/resource/aboutMe.css">
 <?php
 include "../part/head.php";
 ?>
-<h1 class="con">About Me</h1>
+
+
+<div class="aboutme-bn-box">
+    <div class="main-bn">
+        <div style="background-image:url(/resource/images/aboutme1.png);"></div>
+    </div>
+    <div class="main-img">
+        <div style="background-image:url(/resource/images/aboutmeimg2.png);"></div>
+    </div>
+</div>
+
+
+
 <?php
 include "../part/foot.php";
 ?>
