127d851309aabc53e827254e84b782bf61355a91
rimrumrom <yr9810302naver.com>
2020-07-06 18:54:05 +0900

    2020-7-6 작업

--- public/aboutMe.php
+++ public/aboutMe.php
@@ -7,7 +7,7 @@ include "../part/head_head.php";
 include "../part/head.php";
 ?>
 
-
+<!--어바웃미 메인 배너-->
 <div class="aboutme-bn-box">
     <div class="main-bn">
         <div style="background-image:url(/resource/images/aboutme1.png);"></div>
@@ -26,8 +26,14 @@ include "../part/head.php";
     </div>
 </div>
 
+<!--어바웃미-->
+<div class="aboutme">
+    <p></p>
+</div>
 
 
+<p>동해물과 <span style="color:blue; font-weight:bold">백두산이 </span>마르고 닳도록 <span style="color:red;font-style:italic">하나님이 보우하사</span> 우리나라 만세<br>
 <?php
 include "../part/foot.php";
 ?>
+
