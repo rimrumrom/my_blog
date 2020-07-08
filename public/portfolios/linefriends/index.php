ebb4bcad7131e4297db0ad1c242455bbd93ef7a6
rimrumrom <yr981030@naver.com>
2020-07-09 02:57:46 +0900

    2020-7-9 작업

---
+++ public/portfolios/linefriends/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>portfolios</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
+    <link rel="stylesheet" href="index.css">
+    <link rel="icon" href="./resource/images/favicon.png">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="index.js"></script>
+</head>
+
+<body>
+    <div class="top-bar con flex">
+        <a href="#" class="logo"></a>
+        <nav class="menu-box-1 flex-1-0-0">
+            <ul class="flex">
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">CORE BUSINESS</a></li>
+                <li><a href="#">GALLERY</a></li>
+                <li><a href="#">MEDIA</a></li>
+                <li><a href="#">NOTICE</a></li>
+                <li><a href="#">SHOP ONLINE</a></li>
+            </ul>
+        </nav>
+
+        <div class="box">
+            <a href="#">한국어</a>
+            <ul>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="slider slider-1">
+        <div class="slides">
+            <div class="active"
+                style="background-image:url(https://www.linefriends.com/content/banner/201901/b48fa919ea9f40518b77d10f51691602.jpg);">
+            </div>
+            <div
+                style="background-image:url(https://github.com/rimrumrom/img1/blob/master/portfolios/linefriends/f97a2e463c97422496a48ae3cc48ebab.jpg?raw=true);">
+            </div>
+            <div
+                style="background-image:url(https://raw.githubusercontent.com/rimrumrom/img1/master/portfolios/linefriends/063d782f5e99431dacd11bfdc8d56d8d.jpg);">
+            </div>
+            <div
+                style="background-image:url(https://raw.githubusercontent.com/rimrumrom/img1/master/portfolios/linefriends/a546b671f9034b1eabac1d266c1b0f1c.jpg);">
+            </div>
+            <div
+                style="background-image:url(https://github.com/rimrumrom/img1/blob/master/portfolios/linefriends/b702efc697ec4f93b22bcb7c70883ea7.jpg?raw=true);">
+            </div>
+            <div
+                style="background-image:url(https://github.com/rimrumrom/img1/blob/master/portfolios/linefriends/d3996fb2e3994ca0b818a50e958fd855.jpg?raw=true);">
+            </div>
+        </div>
+
+        <div class="pages">
+            <div class="active"></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
+        <div class="side-btns">
+            <div class="prev"></div>
+            <div class="next"></div>
+        </div>
+    </div>
+
+</body>
