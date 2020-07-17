9ad74190fb201e01fdf6ed4342fb95182deb0afe
rimrumrom <yr981030@naver.com>
2020-07-17 17:44:26 +0900

    아트워크,포트폴리오 추가

--- public/portfolios/index.php
+++ public/portfolios/index.php
@@ -9,6 +9,8 @@
 <body>
     <ul>
         <li><a href="/portfolios/linefriends">라인프렌즈</a></li>
+        <li><a href="/portfolios/noncode">논코드</a></li>
+        <li><a href="https://rimrumrom.github.io/img1/portfolios/artworks/1/1.png">아트워크1</a></li>
     </ul>
 </body>
 </html>
