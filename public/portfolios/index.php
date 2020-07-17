40cbadccda64b0330bf2103d83b1512d705b78a0
rimrumrom <yr981030@naver.com>
2020-07-17 18:47:41 +0900

    추가

--- public/portfolios/index.php
+++ public/portfolios/index.php
@@ -1,16 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
-    <ul>
-        <li><a href="/portfolios/linefriends">라인프렌즈</a></li>
-        <li><a href="/portfolios/noncode">논코드</a></li>
-        <li><a href="https://rimrumrom.github.io/img1/portfolios/artworks/1/1.png">아트워크1</a></li>
-    </ul>
+    <h1>코딩 작업물</h1>
+    <nav>
+        <ul>
+            <li>
+                <a href="/portfolios/linefriends">라인프렌즈</a>
+                <ul>
+                    <li><a href="https://www.linefriends.com/">라인프렌즈 원본</a></li>
+                </ul>
+            </li>
+            <ul>
+                <li><a href="/portfolios/noncode">논코드</a>
+                <li><a href="https://noncode.co.kr/">논코드 원본</a></li>
+                </li>
+            </ul>
+    </nav>
+
+    <h1>디자인 작업물</h1>
+    <nav>
+        <ul>
+            <li>
+                <a href="https://rimrumrom.github.io/img1/portfolios/artworks/1/1.png">아트워크1</a>
+                <ul>
+                    <li>창작여부: 모작</li>
+                    <li>출처: 망고보그 <a href="https://www.mangoboard.net/edit/5250468"></a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
 </body>
+
+
 </html>
